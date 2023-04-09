d0b227f7fa5d47fbef701febf34802bdcb235861
robelpop <robelpopandro@gmail.com>
2023-04-09 18:06:08 -0200

    Solve: ranked by their longevity

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+--  script that lists all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT band_name, (COALESCE(split, YEAR(CURDATE()) - 1) - formed) AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', style) > 0
+ORDER BY lifespan DESC;
